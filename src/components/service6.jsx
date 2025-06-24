5f493bb8658fb80e7e817f54764072808662ec94
Prshnt6271 <tinku6271@gmail.com>
2025-06-24 10:38:08 +0530

    utr

--- src/components/service6.jsx
+++ src/components/service6.jsx
@@ -1,27 +1,27 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-// const Service6 = () => {
-//   const navigate = useNavigate();
+const Service6 = () => {
+  const navigate = useNavigate();
 
-//   const handleClick = () => {
-//     navigate("/about");
-//   };
+  const handleClick = () => {
+    navigate("/about");
+  };
 
-//   return (
-//     <section className="min-h-screen bg-[#111111] flex flex-col justify-center items-center text-white text-center px-4">
-//       <h1 className="text-5xl md:text-6xl font-extrabold mb-8">
-//         <span className="text-white">Got a Vision?&apos;s </span>
-//         <span className="text-gray-500">Let’s Make It Real</span>
-//       </h1>
-//       <button
-//         onClick={handleClick}
-//         className="px-6 py-3 text-white font-semibold bg-black border border-[#d8ff85] rounded-full shadow-[0_0_40px_5px_#d8ff85] hover:scale-105 transition-transform duration-300"
-//       >
-//         Get in touch
-//       </button>
-//     </section>
-//   );
-// };
+  return (
+    <section className="min-h-screen bg-[#111111] flex flex-col justify-center items-center text-white text-center px-4">
+      <h1 className="text-5xl md:text-6xl font-extrabold mb-8">
+        <span className="text-white">Got a Vision?&apos;s </span>
+        <span className="text-gray-500">Let’s Make It Real</span>
+      </h1>
+      <button
+        onClick={handleClick}
+        className="px-6 py-3 text-white font-semibold bg-black border border-[#d8ff85] rounded-full shadow-[0_0_40px_5px_#d8ff85] hover:scale-105 transition-transform duration-300"
+      >
+        Get in touch
+      </button>
+    </section>
+  );
+};
 
-// export default Service6;
+export default Service6;
