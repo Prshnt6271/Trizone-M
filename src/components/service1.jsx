0e576b877704502c5791421b90fbcbbab73b8705
Prshnt6271 <tinku6271@gmail.com>
2025-06-24 10:27:59 +0530

    rt

--- src/components/service1.jsx
+++ src/components/service1.jsx
@@ -3,8 +3,9 @@ export default function Service1() {
       <div className="min-h-screen bg-black text-white flex items-center justify-start p-20">
         <div>
           <h1 className="text-8xl font-semibold">
-            Designing Spaces  <br />
-            That Inspire" <br />
+            Unlock Your <br />
+            Potential With <br />
+            Us
           </h1>
           <p className="mt-6 text-gray-400 text-lg">✦ Our Services</p>
           <button className="mt-8 px-6 py-3 rounded-full text-white font-medium text-lg bg-black border border-lime-400 relative overflow-hidden glow-button">
