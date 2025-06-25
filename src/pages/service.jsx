40c5fe694ad33ff0bfb5ee18ea83dd69af5785f2
Prshnt6271 <tinku6271@gmail.com>
2025-06-26 00:43:10 +0530

    er

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -15,7 +15,7 @@ const Services = () => {
   return (
     <div className="bg-[#1b1b1b]">
       <Suspense fallback={<LoadingPlaceholder />}>
-        {/* <Service1 /> */}
+        <Service1 />
         <Service3 />
         <Service44 />
         {/* <Service6 /> */}
