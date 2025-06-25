da393576f519fddca02d5391cf13a164670c8994
Prshnt6271 <tinku6271@gmail.com>
2025-06-26 00:39:46 +0530

    ew

--- src/components/service6.jsx
+++ src/components/service6.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Service6 = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const handleClick = () => {
-  //   navigate("/about");
-  // };
+  const handleClick = () => {
+    navigate("/about");
+  };
 
   return (
     <section className="min-h-screen bg-[#111111] flex flex-col justify-center items-center text-white text-center px-4">
@@ -15,7 +15,7 @@ const Service6 = () => {
         <span className="text-gray-500">Let’s Make It Real</span>
       </h1>
       <button
-        // onClick={handleClick}
+        onClick={handleClick}
         className="px-6 py-3 text-white font-semibold bg-black border border-[#d8ff85] rounded-full shadow-[0_0_40px_5px_#d8ff85] hover:scale-105 transition-transform duration-300"
       >
         Get in touch
