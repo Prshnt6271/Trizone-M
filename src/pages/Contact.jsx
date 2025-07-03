071126eeb3791da06cc6a8dc6b96667f20ec6998
Prshnt6271 <tinku6271@gmail.com>
2025-07-03 12:20:03 +0530

    we

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Contact_Hero from "../components/Contact_Hero";
 import Content from "../components/Content";
 import Map from "../components/Map";
@@ -6,14 +6,31 @@ import Map from "../components/Map";
 function Contact() {
   const [showContent, setShowContent] = useState(false);
 
+  useEffect(() => {
+    if (showContent) {
+      const shouldScroll = sessionStorage.getItem("scrollToMap");
+
+      if (shouldScroll === "true") {
+        // Small delay to ensure Map component has rendered
+        setTimeout(() => {
+          const el = document.getElementById("map");
+          if (el) {
+            el.scrollIntoView({ behavior: "smooth" });
+          }
+        }, 300); // adjust delay if needed
+        sessionStorage.removeItem("scrollToMap");
+      }
+    }
+  }, [showContent]); // run when showContent becomes true
+
   return (
     <>
       <Contact_Hero onAnimationComplete={() => setShowContent(true)} />
       {showContent && (
         <>
           <Content />
-          <Map/>
-          </>
+          <Map />
+        </>
       )}
     </>
   );
